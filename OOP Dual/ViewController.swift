f24b147645afb7bb978dc30fabbfb08c99b4b925
Craig Newcomb <crgnewcomb@gmail.com>
2016-05-06 12:35:41 -0500

    Initial Commit

---
+++ OOP Dual/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OOP Dual
+//
+//  Created by Craig Newcomb on 5/6/16.
+//  Copyright © 2016 CraigNewcomb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ OOP DualTests/OOP_DualTests.swift
@@ -0,0 +1,36 @@
+//
+//  OOP_DualTests.swift
+//  OOP DualTests
+//
+//  Created by Craig Newcomb on 5/6/16.
+//  Copyright © 2016 CraigNewcomb. All rights reserved.
+//
+
+import XCTest
+@testable import OOP_Dual
+
+class OOP_DualTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
